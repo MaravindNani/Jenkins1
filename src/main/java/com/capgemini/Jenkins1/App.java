0a0aca5d67df2226604a91d496086866255a27ff
maaravin <maaravin@LIN24008358.corp.capgemini.com>
2022-03-16 13:31:57 +0530

    change

---
+++ src/main/java/com/capgemini/Jenkins1/App.java
@@ -0,0 +1,18 @@
+package com.capgemini.Jenkins1;
+
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+       String name = "Aravind";
+       if (name == "Aravind") {
+    	   System.out.println("True");
+       }
+       else {
+    	   System.out.println("False");
+       }
+        
+        
+    }
+}
